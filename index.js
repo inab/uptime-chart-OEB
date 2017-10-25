024d8dd2ad75caac58ba8f7e78d5846cd3c91be4
Vicky Sundesha <vsundesh@bsclife010.bsc.es>
2017-10-25 16:00:32 +0200

    http to https

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var jQuery;
 if (window.jQuery === undefined || window.jQuery.fn.jquery !== '3.2.1') {
     var script_tag = document.createElement('script');
     script_tag.setAttribute("type","text/javascript");
-    script_tag.setAttribute("src","http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js");
+    script_tag.setAttribute("src","https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js");
     if (script_tag.readyState) {
       script_tag.onreadystatechange = function () { // For old versions of IE
           if (this.readyState == 'complete' || this.readyState == 'loaded') {
