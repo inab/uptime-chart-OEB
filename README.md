326a319bfd72b30afad57a89ccbc44c71e750df2
Vicky Sundesha <me.vicky@hotmail.com>
2019-10-24 14:36:19 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,41 +1,63 @@
 # UpTime
-This is a widget to display the uptime of a tool at openebench
+
+This is a standalone widget to display the uptime of a tool at OpenEBench.
+
+![Alt text](docs/images/screenshot.png 'Screenshot')
+
+Live demo available at : https://vsundesha.github.io/uptime-chart-OEB/
+
 # How to
+
 Add JS cdn for D3
 Add JS and CSS cdns for c3
+
 ```html
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.3/c3.min.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.3/c3.min.js"></script>
-
 <script src=https://cdnjs.cloudflare.com/ajax/libs/d3/5.9.7/d3.min.js></script>
 ```
-Add the build file which you can download from build/build.js and tag it into your html. The HTML file should look like the following example: 
+
+Add the build file which you can download from build/build.js and tag it into your html. You can then call the `loadChart()` function. The HTML file should look like the following example:
+
+**Note** : The class attribute on the div should be `class="opebuptime"`
+
 ```html
 <!DOCTYPE html>
 <html>
+	<head>
+		<meta charset="utf-8" />
+		<link
+			rel="stylesheet"
+			href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.3/c3.min.css"
+		/>
+	</head>
 
-<head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.3/c3.min.css" />
-</head>
+	<body>
+		<div
+			data-id="test"
+			data-xaxis="true"
+			data-yaxis="false"
+			data-url="biotools:1000genomes_vep:1/web/browser.1000genomes.org"
+			data-limit="22"
+			class="opebuptime"
+		></div>
 
-<body>
-
-  <div data-id="test" data-xaxis="true" data-yaxis="false"
-    data-url="biotools:1000genomes_vep:1/web/browser.1000genomes.org" data-limit="22" class="opebuptime"></div>
-
-  <script src=https://cdnjs.cloudflare.com/ajax/libs/d3/5.9.7/d3.min.js> </script> 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.3/c3.min.js"></script>
-  <script src="build/build.js"></script>
-</body>
-
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.9.7/d3.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.3/c3.min.js"></script>
+		<script src="build/build.js"></script>
+		<script>
+			loadChart();
+		</script>
+	</body>
 </html>
 ```
-### Params
-* data-id : should be unique and start with a letter
-* data-url : from where the data is retrived
-* data-title : chart title
-* data-xaxis : boolean
-* data-w : width of the chart
-* data-h : height of the chart
-* data-limit : amout of dates to be displayed
+
+### Attributes that can be set on the _<div\>_ tag
+
+-   data-id : should be unique and start with a letter
+-   data-url : the ID of the tool from OEB
+-   data-title : chart title
+-   data-xaxis : boolean
+-   data-w : width of the chart
+-   data-h : height of the chart
+-   data-limit : amout of dates to be displayed (min : 5)
--- src/app.js
+++ src/app.js
@@ -5,341 +5,402 @@ import './app.css';
 //./node_modules/.bin/webpack-cli src/app.js --output=build/build.js --module-bind 'css=style-loader!css-loader' -d -w
 
 function genChartData(uptime, divid, xaxis, c_w, c_h, chartTitle) {
-    const dates = [];
-    const code = [];
-    const ac_time = [];
+	const dates = [];
+	const code = [];
+	const ac_time = [];
 
-    for (const x of uptime) {
-        // const date = x.date.split('.')[0].replace('T',' ').replace(/-/g, ".");
-        const date = x.date.split('.')[0].split('T')[0]
-        // Date.parse(date);
-        // console.log(Date.parse(x.date))
-        // const date = x.date;
-        let c;
-        if (x.code == 408 || x.code == 404 || x.code == 301 || x.code == 302 || x.code == 502) {
-            c = 199;
-        } else if (x.code == 200 || x.code == 202) {
-            c = 200;
-        } else if (x.code == null) {
-            c = 201;
-        } else {
-            c = x.code;
-        }
-        if (dates[dates.length - 1] != date) {
-            // console.log(dates[dates.length-1] )
-            dates.push(date);
-            code.push(c);
-            ac_time.push(x.access_time);
-        }
-
-    }
-    dates.unshift('dates');
-    code.unshift('Status');
-    ac_time.unshift('Access Time');
-    populateChart(dates, code, ac_time, divid, xaxis, c_w, c_h, chartTitle)
+	for (const x of uptime) {
+		// const date = x.date.split('.')[0].replace('T',' ').replace(/-/g, ".");
+		const date = x.date.split('.')[0].split('T')[0];
+		// Date.parse(date);
+		// console.log(Date.parse(x.date))
+		// const date = x.date;
+		let c;
+		if (
+			x.code == 408 ||
+			x.code == 404 ||
+			x.code == 301 ||
+			x.code == 302 ||
+			x.code == 502
+		) {
+			c = 199;
+		} else if (x.code == 200 || x.code == 202) {
+			c = 200;
+		} else if (x.code == null) {
+			c = 201;
+		} else {
+			c = x.code;
+		}
+		if (dates[dates.length - 1] != date) {
+			// console.log(dates[dates.length-1] )
+			dates.push(date);
+			code.push(c);
+			ac_time.push(x.access_time);
+		}
+	}
+	dates.unshift('dates');
+	code.unshift('Status');
+	ac_time.unshift('Access Time');
+	populateChart(dates, code, ac_time, divid, xaxis, c_w, c_h, chartTitle);
 }
 
-function populateChart(dates, code, ac_time, divid, xaxis, c_w, c_h, chartTitle) {
-    const tickOptionsX = {
-        // format: '%m-%d',
-        rotate: 90,
-        multiline: false,
-        outer: false,
-    }
-    const tickOptionsY = {
-        outer: false,
-        tooltip: false,
+function populateChart(
+	dates,
+	code,
+	ac_time,
+	divid,
+	xaxis,
+	c_w,
+	c_h,
+	chartTitle
+) {
+	const tickOptionsX = {
+		// format: '%m-%d',
+		rotate: 90,
+		multiline: false,
+		outer: false
+	};
+	const tickOptionsY = {
+		outer: false,
+		tooltip: false
+	};
+	const tickOptionsY2 = {
+		outer: false,
+		format: function(x) {
+			return x % 1 === 0 ? x : '';
+		}
+	};
+	const lowResponseTime = '#ff8214',
+		highResponseTime = '#7C151D',
+		noInfoCaptured = '#cccccc',
+		online = '#8ab803',
+		offline = '#cd5741';
+	// console.log(xaxis)
+	const chart = c3.generate({
+		data: {
+			// xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
+			x: 'dates',
 
-    }
-    const tickOptionsY2 = {
-        outer: false,
-        format: function (x) { return x % 1 === 0 ? x : ''; }
-    }
-    const lowResponseTime = '#ff8214',
-        highResponseTime = '#7C151D',
-        noInfoCaptured = '#cccccc',
-        online = '#8ab803',
-        offline = '#cd5741';
-    // console.log(xaxis)
-    const chart = c3.generate({
-        data: {
-            // xFormat: '%Y-%m-%dT%H:%M:%S.%LZ',
-            x: 'dates',
+			columns: [code, ac_time, dates],
+			types: {
+				'Access Time': 'area',
+				Status: 'bar'
+			},
+			axes: {
+				'Access Time': 'y',
+				Status: 'y2'
+			},
+			colors: {
+				'Access Time': function(d) {
+					// if(d.value){
+					if (d.value >= 240) {
+						return highResponseTime;
+					} else {
+						return lowResponseTime;
+					}
+					// }
+				},
+				Status: function(d) {
+					// if(d.value){
+					if (d.value == 200) {
+						return online;
+					} else if (d.value == 199) {
+						return offline;
+					} else {
+						return noInfoCaptured;
+					}
+				},
+				Online: online,
+				Offline: offline,
+				'No information captured': noInfoCaptured
+				// 'Status': '#588147'
+			},
+			order: false
+		},
+		bar: {
+			width: {
+				ratio: 1
+			}
+		},
+		size: {
+			height: Number(c_h),
+			width: Number(c_w)
+		},
+		title: {
+			text: chartTitle
+		},
+		axis: {
+			y2: {
+				show: false,
+				tick: tickOptionsY2,
+				max: 4000,
+				inverted: false,
+				padding: {
+					left: 0
+				}
+			},
+			y: {
+				show: true,
+				tick: tickOptionsY,
+				min: 0,
 
-            columns: [
-                code,
-                ac_time,
-                dates,
-            ],
-            types: {
+				label: {
+					text: 'Access time in milliseconds',
+					position: 'outer-center'
+				},
 
-                'Access Time': 'area',
-                'Status': 'bar',
-            },
-            axes: {
-                'Access Time': 'y',
-                'Status': 'y2',
-            },
-            colors: {
-                'Access Time': function (d) {
-                    // if(d.value){
-                    if (d.value >= 240) {
-                        return highResponseTime;
-                    } else {
-                        return lowResponseTime;
-                    };
-                    // }
-                },
-                'Status': function (d) {
-                    // if(d.value){
-                    if (d.value == 200) {
-                        return online;
-                    } else if (d.value == 199) {
-                        return offline;
-                    } else {
-                        return noInfoCaptured;
-                    }
-                },
-                'Online': online,
-                'Offline': offline,
-                'No information captured': noInfoCaptured
-                // 'Status': '#588147'
-            },
-            order: false,
+				// inverted:false,
+				padding: {
+					bottom: 11
+				}
+			},
 
-        },
-        bar: {
-            width: {
-                ratio: 1,
-            }
-        },
-        size: {
-            height: Number(c_h),
-            width: Number(c_w),
-        },
-        title: {
-            text: chartTitle,
-        },
-        axis: {
-            y2: {
-                show: false,
-                tick: tickOptionsY2,
-                max: 4000,
-                inverted: false,
-                padding: {
-                    left: 0,
-                },
+			x: {
+				type: 'category',
+				// type : 'timeseries',
+				tick: tickOptionsX,
+				label: {
+					text: 'Dates',
+					position: 'outer-right'
+				},
+				padding: {
+					right: 2
+				}
+			}
+		},
+		subchart: {
+			show: false
+		},
+		padding: {
+			top: 0
+		},
+		legend: {
+			padding: 5,
+			item: {
+				tile: {
+					width: 15,
+					height: 2
+				}
+			},
+			show: false,
+			position: 'bottom'
+		},
+		bindto: '#' + divid,
+		tooltip: {
+			contents: function(
+				d,
+				defaultTitleFormat,
+				defaultValueFormat,
+				color
+			) {
+				let $$ = this,
+					config = $$.config,
+					titleFormat =
+						config.tooltip_format_title || defaultTitleFormat,
+					nameFormat =
+						config.tooltip_format_name ||
+						function(name) {
+							return name;
+						},
+					valueFormat =
+						config.tooltip_format_value || defaultValueFormat,
+					text,
+					i,
+					title,
+					value,
+					name,
+					bgcolor,
+					total = 0;
 
-            },
-            y: {
-                show: true,
-                tick: tickOptionsY,
-                min: 0,
+				for (i = 0; i < d.length; i++) {
+					total = total + d[i].value;
+					if (!(d[i] && (d[i].value || d[i].value === 0))) {
+						continue;
+					}
 
-                label: {
-                    text: 'Access time in milliseconds',
-                    position: 'outer-center'
-                },
+					if (!text) {
+						title = titleFormat ? titleFormat(d[i].x) : d[i].x;
+						text =
+							"<table class='" +
+							$$.CLASS.tooltip +
+							"'>" +
+							(title || title === 0
+								? "<tr><th colspan='2'>" + title + '</th></tr>'
+								: '');
+					}
 
-                // inverted:false,
-                padding: {
-                    bottom: 11,
+					name = nameFormat(
+						d[i].name,
+						d[i].ratio,
+						d[i].id,
+						d[i].index
+					);
+					value = valueFormat(
+						d[i].value,
+						d[i].ratio,
+						d[i].id,
+						d[i].index
+					);
+					// console.log(name, value)
+					// bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
+					// bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : d[i].value>160?'#FFD300' : '#588147';
+					if (name == 'Access Time') {
+						if (d[i].value >= 160) {
+							bgcolor = highResponseTime;
+						} else {
+							bgcolor = lowResponseTime;
+						}
+					} else {
+						if (d[i].value == 200) {
+							bgcolor = online;
+						} else if (d[i].value == 199) {
+							bgcolor = offline;
+						} else {
+							bgcolor = noInfoCaptured;
+						}
+					}
 
-                },
+					text +=
+						"<tr class='" +
+						$$.CLASS.tooltipName +
+						'-' +
+						d[i].id +
+						"'>";
+					text +=
+						"<td class='name'><span style='background-color:" +
+						bgcolor +
+						"'></span>" +
+						name +
+						'</td>';
 
-            },
+					if (name == 'Access Time') {
+						text += "<td class='value'>" + value + 'ms </td>';
+					} else {
+						if (value == 200) {
+							text += "<td class='value'> Online </td>";
+						} else if (value == 199) {
+							text += "<td class='value'> Offline </td>";
+						} else if (value == 201) {
+							text +=
+								"<td class='value'> No information captured </td>";
+						} else {
+							text += "<td class='value'>" + value + '</td>';
+						}
+					}
 
-            x: {
-                type: 'category',
-                // type : 'timeseries',
-                tick: tickOptionsX,
-                label: {
-                    text: 'Dates',
-                    position: 'outer-right'
-                },
-                padding: {
-                    right: 2,
+					text += '</tr>';
+				}
+				return text + '</table>';
+			}
+		}
+	});
 
-                }
+	d3.select('#' + divid)
+		.insert('div', '.chart')
+		.attr('class', 'oeb-legend')
+		.insert('div', '.chart')
+		.attr('class', 'legend-scale')
+		.insert('ul', '.chart')
+		.attr('class', 'legend-labels')
 
-            },
+		.selectAll('span')
 
-        },
-        subchart: {
-            show: false,
-        },
-        padding: {
-            top: 0,
-        },
-        legend: {
-            padding: 5,
-            item: {
-                tile: {
-                    width: 15,
-                    height: 2
-                },
-            },
-            show: false,
-            position: 'bottom',
-        },
-        bindto: '#' + divid,
-        tooltip: {
-            contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+		.data(['Online', 'Offline', 'No information captured', 'Access Time'])
+		.enter()
+		.append('li')
+		.html(function(id) {
+			return id;
+		})
+		.append('span')
+		.attr('data-id', function(id) {
+			return id;
+		})
 
-
-                let $$ = this, config = $$.config,
-
-                    titleFormat = config.tooltip_format_title || defaultTitleFormat,
-                    nameFormat = config.tooltip_format_name || function (name) { return name; },
-                    valueFormat = config.tooltip_format_value || defaultValueFormat,
-                    text, i, title, value, name, bgcolor, total = 0;
-
-                for (i = 0; i < d.length; i++) {
-
-                    total = total + d[i].value;
-                    if (!(d[i] && (d[i].value || d[i].value === 0))) { continue; }
-
-                    if (!text) {
-                        title = titleFormat ? titleFormat(d[i].x) : d[i].x;
-                        text = "<table class='" + $$.CLASS.tooltip + "'>" + (title || title === 0 ? "<tr><th colspan='2'>" + title + "</th></tr>" : "");
-                    }
-
-
-                    name = nameFormat(d[i].name, d[i].ratio, d[i].id, d[i].index);
-                    value = valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index);
-                    // console.log(name, value)
-                    // bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
-                    // bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : d[i].value>160?'#FFD300' : '#588147';
-                    if (name == 'Access Time') {
-
-                        if (d[i].value >= 160) {
-                            bgcolor = highResponseTime
-                        } else {
-                            bgcolor = lowResponseTime
-                        };
-                    } else {
-                        if (d[i].value == 200) {
-                            bgcolor = online
-                        } else if (d[i].value == 199) {
-                            bgcolor = offline
-                        } else {
-                            bgcolor = noInfoCaptured
-                        };
-                    };
-
-                    text += "<tr class='" + $$.CLASS.tooltipName + "-" + d[i].id + "'>";
-                    text += "<td class='name'><span style='background-color:" + bgcolor + "'></span>" + name + "</td>";
-
-                    if (name == 'Access Time') {
-                        text += "<td class='value'>" + value + "ms </td>";
-                    }
-                    else {
-                        if (value == 200) {
-                            text += "<td class='value'> Online </td>";
-                        } else if (value == 199) {
-                            text += "<td class='value'> Offline </td>";
-                        } else if (value == 201) {
-                            text += "<td class='value'> No information captured </td>";
-                        } else {
-                            text += "<td class='value'>" + value + "</td>";
-                        }
-
-                    }
-
-                    text += "</tr>";
-                }
-                return text + "</table>";
-            },
-        }
-    });
-
-    d3.select('#'+divid).insert('div', '.chart').attr('class', 'oeb-legend')
-    .insert('div','.chart').attr('class','legend-scale')
-    .insert('ul','.chart').attr('class','legend-labels')
-    
-    .selectAll('span')
-
-    .data(['Online','Offline','No information captured', 'Access Time'])
-    .enter().append('li').html(function (id) { return id; }).append('span')
-    .attr('data-id', function (id) { return id; })
-    
-    .each(function (id) {
-        d3.select(this).style('background-color', chart.color(id));
-    })
+		.each(function(id) {
+			d3.select(this).style('background-color', chart.color(id));
+		});
 }
-
 
 async function loadChart(elems) {
+	// console.log(elems);
+	if (elems === undefined) {
+		elems = document.getElementsByClassName('opebuptime');
+	}
 
-    // console.log(elems);
-    if (elems === undefined) {
-        elems = document.getElementsByClassName("opebuptime");
-    }
+	let i = 0;
+	for (let y of elems) {
+		try {
+			i++;
+			const dataId = y.getAttribute('data-id');
+			const chartUrl = y.getAttribute('data-url');
+			const mode = y.getAttribute('dev');
+			let chartTitle = y.getAttribute('data-title');
+			if (!chartTitle) {
+				chartTitle = '';
+			}
+			let xaxis = y.getAttribute('data-xaxis');
+			if (!xaxis) {
+				xaxis = 'false';
+			}
 
-    let i = 0;
-    for (let y of elems) {
-        try {
-            i++;
-            const dataId = y.getAttribute('data-id');
-            const chartUrl = y.getAttribute('data-url');
-            const mode = y.getAttribute('dev')
-            let chartTitle = y.getAttribute('data-title');
-            if (!chartTitle) {
-                chartTitle = '';
-            }
-            let xaxis = y.getAttribute('data-xaxis');
-            if (!xaxis) {
-                xaxis = "false";
-            }
-
-            let c_w = y.getAttribute('data-w');
-            if (c_w < 200 || c_w == 0 || !c_w) {
-                c_w = null;
-            }
-            let c_h = y.getAttribute('data-h');
-            if (c_h < 200 || c_h == 0 || !c_h) {
-                c_h = null;
-            }
-            let limit = y.getAttribute('data-limit');
-            if (!limit || limit < 5) {
-                limit = 5;
-            }
-            const div = document.createElement("div");
-            const divid = dataId + i;
-            div.id = divid;
-            y.appendChild(div)
-            genChartData(await fetchUrl(chartUrl, limit, mode), divid, xaxis, c_w, c_h, chartTitle)
-
-        } catch (err) {
-            console.log('Internat error :' + err)
-        }
-    }
+			let c_w = y.getAttribute('data-w');
+			if (c_w < 200 || c_w == 0 || !c_w) {
+				c_w = null;
+			}
+			let c_h = y.getAttribute('data-h');
+			if (c_h < 200 || c_h == 0 || !c_h) {
+				c_h = null;
+			}
+			let limit = y.getAttribute('data-limit');
+			if (!limit || limit < 5) {
+				limit = 5;
+			}
+			const div = document.createElement('div');
+			const divid = dataId + i;
+			div.id = divid;
+			y.appendChild(div);
+			genChartData(
+				await fetchUrl(chartUrl, limit, mode),
+				divid,
+				xaxis,
+				c_w,
+				c_h,
+				chartTitle
+			);
+		} catch (err) {
+			console.log('Internat error :' + err);
+		}
+	}
 }
 async function fetchUrl(url, limit, mode) {
-    try {
-        let base_url;
-        if (mode != null) {
-            base_url = "dev-openebench"
-        } else {
-            base_url = "openebench"
-        }
+	try {
+		let base_url;
+		if (mode != null) {
+			base_url = 'dev-openebench';
+		} else {
+			base_url = 'openebench';
+		}
 
-        // console.log("https://openebench.bsc.es/monitor/rest/homepage/"+url+"?limit="+limit)
-        var currentDate = Math.floor(new Date().getTime()/1000);
-        var startDate = date2-(limit*86400);
+		let currentDate = Math.floor(new Date().getTime() / 1000);
+		let startDate = currentDate - limit * 86400;
 
-        console.log(date2)
-        let request = await fetch("https://" + base_url + ".bsc.es/monitor/rest/homepage/" + url + "?date1="+startDate+"&date2="+ currentDate)
-        let result = await request.json();
-        return result;
+		let request = await fetch(
+			'https://' +
+				base_url +
+				'.bsc.es/monitor/rest/homepage/' +
+				url +
+				'?date1=' +
+				startDate +
+				'&date2=' +
+				currentDate
+		);
 
-    }
-    catch (err) {
-        console.log(`Invalid Url Error: ${err.stack} `);
-    }
+		let result = await request.json();
+		return result;
+	} catch (err) {
+		console.log(`Invalid Url Error: ${err.stack} `);
+	}
 }
 
-export {
-    loadChart
-}
+export { loadChart };
 loadChart();
